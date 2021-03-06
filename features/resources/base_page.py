fed1cf650f72a7530561aa97c5c5907e9f6a5d47
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2021-03-06 00:31:08 -0300

    page base of functions
    page base of functions

---
+++ features/resources/base_page.py
@@ -0,0 +1,39 @@
+from PageObjectLibrary import PageObject
+import re
+
+class BasePage(PageObject):
+
+    def __init__(self, timeout=30):
+        super().__init__()
+        self.wait_timeout = timeout
+
+    def wait_element_iterable(self, element_to_interact):
+        self.selib.wait_until_page_contains_element(element_to_interact, self.wait_timeout)
+        self.selib.wait_until_element_is_visible(element_to_interact, self.wait_timeout)
+
+    def click_element(self, element_to_interact):
+        self.wait_element_iterable(element_to_interact)
+        self.selib.click_element(element_to_interact)
+    
+    def check_element_is_visible(self, element_to_interact):
+        self.wait_element_iterable(element_to_interact)
+
+    def input_text(self, element_to_interact, text):
+        self.wait_element_iterable(element_to_interact)
+        self.selib.input_text(element_to_interact, text)
+
+    def select_from_list_by_index(self, element_to_interact, index):
+        self.wait_element_iterable(element_to_interact)
+        self.selib.select_from_list_by_index(element_to_interact, index)
+
+    def select_from_list_by_value(self, element_to_interact, value):
+        self.wait_element_iterable(element_to_interact)
+        self.selib.select_from_list_by_value(element_to_interact, value)
+
+    def get_value(self, element_to_interact):
+        self.wait_element_iterable(element_to_interact)
+        self.selib.get_text(element_to_interact)
+
+    def convert_to_float(self, value):
+        value_output = re.findall("\d*,\d*", value)
+        return float(value_output[0].replace(',', '.'))
