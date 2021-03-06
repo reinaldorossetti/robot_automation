5b8a9055cf8feb7097863728947f398fc8926354
reinaldorossetti <reinaldo.rossetti@outlook.com>
2021-03-06 19:34:05 -0300

    change funtion name

--- features/pages/HomePage.py
+++ features/pages/HomePage.py
@@ -16,13 +16,13 @@ class HomePage(BasePage):
         self.click_element(self.locator.button_age_gate_yes)
 
     def inform_address(self, address):
-        self.exec_javascript(self.locator.search_address, "focus")
+        self.trigger_event(self.locator.search_address, "focus")
         self.input_text(self.locator.input_address, address)
         self.click_element(self.locator.address)
         self.input_text(self.locator.address_details_input_complement, 'Na Casa de Esquina')
         self.click_element(self.locator.adress_button_continue)
 
-    def exec_javascript(self, element_to_interact, trigger_event):
+    def trigger_event(self, element_to_interact, trigger_event):
         path = os.getcwd()
         with open(path + '/features/resources/jquery.min.js', 'r') as jquery_js:  # read the jquery from a file
             jquery = jquery_js.read()
